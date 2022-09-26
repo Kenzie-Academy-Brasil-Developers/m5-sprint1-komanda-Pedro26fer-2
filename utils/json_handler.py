4aac32617401dc7441318d914426678ebf5aaa93
Pedro26fer <p2698fernandes@gmail.com>
2022-09-26 12:54:50 -0300

    resolving bugs on github tests

--- utils/json_handler.py
+++ utils/json_handler.py
@@ -1,5 +1,5 @@
 import json
-from textwrap import indent
+
 
 
 def read_json(databse_path):
@@ -16,15 +16,39 @@ def read_json(databse_path):
 
 
 
-def write_json(database_path, new_plate: dict):
-    
-    with open(database_path, "r", encoding="utf8") as menu:
-        file = json.load(menu)
-        new_plate = {**new_plate, "id": len(file)+1}
-        file = [*file, new_plate ]
-        with open(database_path, "w", encoding="utf8") as file_json:
-            json.dump(file, file_json, indent= 4)
+def write_json(new_plate: dict, database_path = 'menu.json'):
 
-    return file[-1]    
+    try:
+         with open(database_path, "r", encoding="utf8") as menu:
+
+            file = json.load(menu)
+
+            if type(file) == dict:
+
+                file_list = list()
+                file_list.append(file)
+                new_plate = {**new_plate, "id": len(file_list)+1}
+                file_list.append(new_plate)
+                with open(database_path, "w", encoding="utf8") as file_json:
+                    json.dump(file_list, file_json, indent= 4) 
+                return file_list[-1] 
+
+            new_plate = {**new_plate, "id": len(file)+1}
+            file = [*file, new_plate]
+
+            with open(database_path, "w", encoding="utf8") as file_json:
+                    json.dump(file, file_json, indent= 4) 
+            return file[-1]          
+
+
+    except IOError:
+            first_plate = {**new_plate, "id": 1}
+            with open(database_path, "w", encoding="utf8") as file_json:
+                json.dump(first_plate, file_json, indent= 4)
+                return first_plate
+
+
+        
+
       
 
